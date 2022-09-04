0402570f694211a81ee095c79f16a1a2302f80fd
Tao Ren <32982291+cliffren@users.noreply.github.com>
2022-09-04 10:07:30 +0800

    update examples

--- examples/data_download.R
+++ examples/data_download.R
@@ -5,13 +5,17 @@ devtools::install_github('satijalab/seurat-data')
 library(SeuratData)
 library(SeuratDisk)
 
+options(timeout=3000)
+
 dir.create('./data')
-data_url <- 'https://xialab.s3-us-west-2.amazonaws.com/Tao/PENCIL_data/'
+data_url <- 'https://xialab.s3.us-west-2.amazonaws.com/tools/PENCIL/'
 
 # loading simulaiton_data_1
-data_name <- 'Feldman_T_cell_data_simulation'
-download.file(sprintf('%s%s.RData', data_url, data_name), sprintf('./data/%s.RData', data_name))
-load(sprintf('./data/%s.RData', data_name))
+data_name <- 'PENCIL_tutorial_1'
+if (!file.exists(sprintf('./data/%s.Rdata', data_name))){
+    download.file(sprintf('%s%s.Rdata', data_url, data_name), sprintf('./data/%s.Rdata', data_name))
+}
+load(sprintf('./data/%s.Rdata', data_name))
 SaveH5Seurat(sc_data, filename = sprintf('./data/%s.h5Seurat', data_name))
 Convert(sprintf('./data/%s.h5Seurat', data_name), dest='h5ad')
 mvg2000 <- VariableFeatures(sc_data)
@@ -20,11 +24,13 @@ file.remove(sprintf('./data/%s.h5Seurat', data_name))
 
 
 # loading simulaiton_data_2
-data_name <- 'DAseq_tutorial_data_simulation'
-download.file(sprintf('%s%s.RData', data_url, data_name), sprintf('./data/%s.RData', data_name))
-load(sprintf('./data/%s.RData', data_name))
+data_name <- 'PENCIL_tutorial_2'
+if (!file.exists(sprintf('./data/%s.Rdata', data_name))){
+    download.file(sprintf('%s%s.Rdata', data_url, data_name), sprintf('./data/%s.Rdata', data_name))
+}
+load(sprintf('./data/%s.Rdata', data_name))
 sc_data.2@reductions$pca <- NULL
-SaveH5Seurat(sc_data, filename = sprintf('./data/%s.h5Seurat', data_name))
+SaveH5Seurat(sc_data.2, filename = sprintf('./data/%s.h5Seurat', data_name))
 Convert(sprintf('./data/%s.h5Seurat', data_name), dest='h5ad')
 pcs_list <- row.names(sc_data.2)
 write.csv(pcs_list, sprintf('./data/%s_pcs_list.csv', data_name))
