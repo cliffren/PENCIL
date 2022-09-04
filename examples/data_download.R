68bb635ec037a8a79f9712c213721de1a9824dd7
Tao Ren <32982291+cliffren@users.noreply.github.com>
2022-09-04 10:10:36 +0800

    Update data_download.R

--- examples/data_download.R
+++ examples/data_download.R
@@ -10,7 +10,7 @@ options(timeout=3000)
 dir.create('./data')
 data_url <- 'https://xialab.s3.us-west-2.amazonaws.com/tools/PENCIL/'
 
-# loading simulaiton_data_1
+# downloading simulaiton_data_1
 data_name <- 'PENCIL_tutorial_1'
 if (!file.exists(sprintf('./data/%s.Rdata', data_name))){
     download.file(sprintf('%s%s.Rdata', data_url, data_name), sprintf('./data/%s.Rdata', data_name))
@@ -23,7 +23,7 @@ write.csv(mvg2000, sprintf('./data/%s_mvg2000_list.csv', data_name))
 file.remove(sprintf('./data/%s.h5Seurat', data_name))
 
 
-# loading simulaiton_data_2
+# downloading simulaiton_data_2
 data_name <- 'PENCIL_tutorial_2'
 if (!file.exists(sprintf('./data/%s.Rdata', data_name))){
     download.file(sprintf('%s%s.Rdata', data_url, data_name), sprintf('./data/%s.Rdata', data_name))
